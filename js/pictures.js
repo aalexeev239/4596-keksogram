31df608d48bcf7465e60139ab769625616998ad4
aalexeev <alexeev.andrey.a@gmail.com>
2015-10-02 00:40:13 +0300

    add xhr request && filter events

--- js/pictures.js
+++ js/pictures.js
@@ -1,32 +1,90 @@
 var pictureTemplate = (function() {
   'use strict';
 
-  var filtersBlock,
-      picturesBlock,
-      template,
-      IMAGE_FAILURE_TIMEOUT = 10000;
+  var filtersBlock;
+  var picturesBlock;
+  var template;
+  var IMAGE_FAILURE_TIMEOUT = 10000;
+  var REQUEST_FAILURE_TIMEOUT = 10000;
+  var ReadyState = {
+    'UNSENT': 0,
+    'OPENED': 1,
+    'HEADERS_RECEIVED': 2,
+    'LOADING': 3,
+    'DONE': 4
+  };
+  // one month
+  var FILTER_NEW_AMOUNT = 1*30*24*60*60*1000
 
 
   var pictureTemplate = {
-    init: function(){
-      var frag = document.createDocumentFragment();
+    init: function() {
 
+      // setting up global DOM objects
       filtersBlock = document.querySelector('.filters');
       picturesBlock = document.querySelector('.pictures');
       template = document.getElementById('picture-template');
 
       if (!filtersBlock || !picturesBlock || !template) return;
 
+
       filtersBlock.classList.add('hidden');
 
-      pictures.forEach(function(picture, i) {
-        frag.appendChild(pictureTemplate.getHTML(picture));
+
+      this.loadData(this.onLoadSuccess,this.onLoadFailure);
+    },
+    loadData: function(success, failure) {
+
+      var xhr = new XMLHttpRequest();
+      xhr.timeout = REQUEST_FAILURE_TIMEOUT;
+      xhr.open('get', 'data/pictures.json');
+
+      xhr.onreadystatechange = function(ev) {
+        var loadedxhr = ev.target;
+
+        switch (loadedxhr.readyState) {
+          case ReadyState.OPENED:
+          case ReadyState.HEADERS_RECEIVED:
+          case ReadyState.LOADING:
+            picturesBlock.classList.add('pictures-loading');
+            break;
+
+          case ReadyState.DONE:
+          default:
+            picturesBlock.classList.remove('pictures-loading');
+
+            if (loadedxhr.status === 200) {
+              var data = loadedxhr.response;
+              success(JSON.parse(data));
+            } else {
+              failure();
+            }
+        }
+      }
+
+      xhr.send();
+    },
+    onLoadFailure: function() {
+      picturesBlock.classList.add('pictures-failure');
+    },
+    onLoadSuccess: function(data) {
+      pictureTemplate.renderPictures(data);
+      pictureTemplate.setupFilters(data);
+      filtersBlock.classList.remove('hidden');
+    },
+    renderPictures: function(data) {
+      var frag = document.createDocumentFragment();
+
+      // clearing pictureBlock
+      picturesBlock.innerHTML = '';
+
+      data.forEach(function(picture, i) {
+        frag.appendChild(pictureTemplate.getPictureHTML(picture));
       });
 
       picturesBlock.appendChild(frag);
-      filtersBlock.classList.remove('hidden');
     },
-    getHTML: function(picture) {
+    getPictureHTML: function(picture) {
       function imgFailure(){
         picImg.classList.add('picture-load-failure');
       }
@@ -68,8 +126,48 @@ var pictureTemplate = (function() {
 
 
       return pic;
+    },
+    setupFilters: function(pictures) {
+      var filterForm = document.querySelector('.filters');
+
+      if (!filterForm.filter) return;
+
+      filterForm.addEventListener('change', function(e) {
+        pictureTemplate.renderPictures(pictureTemplate.applyFilter(pictures, filterForm.filter.value));
+      });
+    },
+    applyFilter: function(items, val) {
+      var res;
+      switch (val) {
+        case 'new':
+          res = items.filter(function(a){
+            var dateA = Date.parse(a.date);
+            var now = Date.now();
+
+            return now - dateA < FILTER_NEW_AMOUNT;
+          }).sort(function(a,b){
+            var dateA = Date.parse(a.date);
+            var dateB = Date.parse(b.date);
+
+            return dateB - dateA;
+          });
+
+        break;
+
+        case 'discussed':
+          res = items.sort(function(a,b){
+            return b.comments - a.comments;
+          });
+
+        default:
+          res = items.slice(0);
+      }
+
+      return res;
     }
   };
+
+
 
   return pictureTemplate;
 }());
@@ -77,5 +175,5 @@ var pictureTemplate = (function() {
 
 // initing module
 document.addEventListener("DOMContentLoaded", function(){
-  pictures && pictureTemplate.init();
+  pictureTemplate.init();
 });
