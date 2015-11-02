00465458aa9c5b7fd3a2ceab5d1158e41b186269
Andrey Alexeev <alexeev.andrey.a@gmail.com>
2015-11-02 03:03:58 +0300

    fix Каждый раз когда ты вызываешь uploadPicture, у тебя заново создадутся новые loadSuccess, loadError и removeListeners, то есть к каждой новой картинке будут привязаны три новых функции.

--- js/views/photo.js
+++ js/views/photo.js
@@ -17,7 +17,6 @@
   var PICTURE_SIZE = 182;
 
 
-
   /**
    * template to render
    * @type {Element}
@@ -49,17 +48,38 @@
      */
     className: 'picture',
 
+
     /**
      * rendering
      * @override
      */
     render: function() {
+      this._onImageLoaded = this._onImageLoaded.bind(this);
+      this._onImageFailed = this._onImageFailed.bind(this);
+
+
       this.el.appendChild(template.content.cloneNode(true));
       this.el.querySelector('.picture-likes').textContent = this.model.get('likes');
       this.el.querySelector('.picture-comments').textContent = this.model.get('comments');
       this.el.querySelector('img');
-      _uploadPicture(this.model.get('url'), this.el.querySelector('img'));
+
+      // _uploadPicture(this.model.get('url'), this.el.querySelector('img'));
+      if (this.model.get('url')) {
+        var img = new Image();
+
+        img.addEventListener('load', this._onImageLoaded);
+        img.addEventListener('error', this._onImageFailed);
+        img.addEventListener('abort', this._onImageFailed);
+
+        img.src = this.model.get('url');
+
+        this._imageLoadTimeout = setTimeout((function() {
+          this.el.classList.add('picture-load-failure');
+        }).bind(this), REQUEST_FAILURE_TIMEOUT);
+      }
     },
+
+
     /**
      * mapping events
      * @type {Object.<string, string>}
@@ -67,64 +87,63 @@
     events: {
       'click': '_onClick'
     },
+
+
     /**
      * click handler, triggering 'galleryclick' event
      * @param   {MouseEvent} ev
      * @private
      */
     _onClick: function() {
-      if (!this.el.querySelector('.picture-load-failure')) {
+      if (!this.el.classList.contains('picture-load-failure')) {
         this.trigger('galleryclick');
       }
-    }
-  });
+    },
 
 
+    /**
+     * fires after image is loaded
+     * @param   {Event} ev
+     * @private
+     */
+    _onImageLoaded: function(ev) {
+      clearTimeout(this._imageLoadTimeout);
+      var img = ev.path[0];
+      var currentImg = this.el.querySelector('img');
+      this.el.classList.remove('picture-load-failure'); // if loaded after timeout is passed
 
-  /**
-   * uploading Picture Helper
-   * @param   {string} url
-   * @param   {Element} node
-   * @private
-   */
-  function _uploadPicture(url, node) {
-    var img = new Image();
-    var timer;
+      this._cleanupImageListeners(img);
 
-    function _loadSuccess() {
-      clearTimeout(timer);
-      node.parentNode.replaceChild(img, node);
+      currentImg.parentNode.replaceChild(img, currentImg);
       img.style.width = PICTURE_SIZE;
       img.style.hight = PICTURE_SIZE;
-      _removeListeners();
+    },
+
+
+    /**
+     * fires after image load is failed
+     * @param   {Event} ev
+     * @private
+     */
+    _onImageFailed: function(ev) {
+      clearTimeout(this._imageLoadTimeout);
+      var img = ev.path[0];
+      this._cleanupImageListeners(img);
+      this.el.classList.add('picture-load-failure');
+    },
+
+
+    /**
+     * remove listeners from image
+     * @param   {Image} img
+     * @private
+     */
+    _cleanupImageListeners: function(img) {
+      img.removeEventListener('load', this._onImageLoaded);
+      img.removeEventListener('error', this._onImageFailed);
+      img.removeEventListener('abort', this._onImageFailed);
     }
-
-    function _loadError() {
-      clearTimeout(timer);
-      node.classList.add('picture-load-failure');
-      _removeListeners();
-    }
-
-
-    function _removeListeners() {
-      img.removeEventListener('load', _loadSuccess);
-      img.removeEventListener('error', _loadError);
-      img.removeEventListener('abort', _loadError);
-    }
-
-
-
-    timer = setTimeout(function() {
-      _loadError();
-    }, REQUEST_FAILURE_TIMEOUT);
-
-    img.addEventListener('load', _loadSuccess);
-    img.addEventListener('error', _loadError);
-    img.addEventListener('abort', _loadError);
-
-    img.src = url;
-  }
-
+  });
 
 
   window.PhotoView = PhotoView;
